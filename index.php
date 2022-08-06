dc9ce9cfe71e70856c9978e4d7c4e089fb4bae90
IFAN RIFALDI <102780600+ifanrifaldi@users.noreply.github.com>
2022-08-06 09:52:15 +0700

    Menambahkan Kelas Anjing

--- index.php
+++ index.php
@@ -19,3 +19,20 @@ echo "Jumlah_Kaki : $kucing->jumlah_kaki <br>";
 echo "Bisa_Terbang : $kucing->bisa_terbang <br>";
 echo "Suara : $kucing->suara <br>";
 echo "<hr>"; 
+
+class Anjing{
+    public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$anjing = new anjing;
+$anjing->Nama = "Marco";
+$anjing->Jumlah_Kaki = "4";
+$anjing->Bisa_Terbang ="tidak";
+$anjing->Suara = "Guk-guk";
+
+echo "anjing <br>";
+echo "Nama : $anjing->Nama <br>";
+echo "jumlah_kaki : $anjing->Jumlah_Kaki <br>";
+echo "bisa_terbang : $anjing->Bisa_Terbang <br>";
+echo "suara : $anjing->Suara <br>";
+echo "<hr>";
